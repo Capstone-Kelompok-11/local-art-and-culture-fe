3e72d264f4feac87a186624f6e7e9b144c332ce1
faurrahman10 <faturrahmanhasir@gmail.com>
2023-11-20 15:55:36 +0800

    buat route untuk login

--- lokasani/src/App.jsx
+++ lokasani/src/App.jsx
@@ -1,11 +1,16 @@
-import './App.css'
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Login from "./pages/login";
+import Home from "./pages/home";
 
 function App() {
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Home/>} path="/"/>
+        <Route element={<Login/>} path="/auth/login"/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
