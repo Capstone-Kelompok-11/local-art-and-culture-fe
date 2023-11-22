3b45ecbf3f45b0e7d930145f24afb93af821488e
faurrahman10 <faturrahmanhasir@gmail.com>
2023-11-22 14:47:59 +0800

    mengimport component ke home dan memberikan sedikit tambahan style

--- lokasani/src/pages/Home.jsx
+++ lokasani/src/pages/Home.jsx
@@ -1,36 +1,47 @@
-import React from "react";
 import Sidebar from "../component/Sidebar";
 import Header from "../component/Header";
-import ChartPenjualan from "../component/ChartPenjualan";
+import Penjualan from "../component/Penjualan";
+import ChartPenghasilan from "../component/ChartPenghasilan";
 import GrafikBulanan from "../component/GrafikBulanan";
 import Prospek from "../component/Prospek";
+import Statistik from "../component/Statistik";
 
 const Home = () => {
-  return (
-    <div className="">
-      <div className="flex">
-        <div>
-          <Sidebar />
+    return (
+        <div className="bg-[#F2F2F2]">
+            <div className="flex">
+                <div>
+                    <Sidebar />
+                </div>
+                <div className="w-full pl-[255px]">
+                    <div>
+                        <Header />
+                    </div>
+					<div className="flex gap-6 pt-20 px-5">
+						<div className="w-2/5">
+							<Penjualan />
+						</div>
+						<div className="w-3/5">
+							<Statistik/>
+						</div>
+					</div>
+                    <div className="flex gap-6 w-full px-5 mt-6">
+                        <div className="w-2/5">
+                            <div>
+                                <GrafikBulanan />
+                            </div>
+                            <div>
+                                <Prospek />
+                            </div>
+                        </div>
+						<div className="w-3/5">
+							<ChartPenghasilan />
+						</div>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div className="ml-[353px]">
-          <div>
-            <Header />
-          </div>
-          <div className="h-[2000px] pt-20 ml-10 ">
-            <div>
-              <ChartPenjualan />
-            </div>
-            <div>
-              <GrafikBulanan />
-            </div>
-            <div>
-              <Prospek />
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Home;
