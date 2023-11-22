5f5213513f0236499724472223147c76eb4c41bb
faurrahman10 <faturrahmanhasir@gmail.com>
2023-11-22 14:47:11 +0800

    memberikan style tambahan dan mengganti icon ke material icon

--- lokasani/src/component/Sidebar.jsx
+++ lokasani/src/component/Sidebar.jsx
@@ -1,81 +1,150 @@
-import React from "react";
-import {
-  MdChat,
-  MdHome,
-  MdShoppingCart,
-  MdReceipt,
-  MdConveyorBelt,
-  MdGroup,
-  MdQuestionAnswer,
-  MdLogout,
-} from "react-icons/md";
+/* eslint-disable no-unused-vars */
+import React, {useState} from 'react'
+import {  useNavigate } from "react-router-dom";
+import HomeIcon from '@mui/icons-material/Home';
+import ChatIcon from '@mui/icons-material/Chat';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import EventNoteIcon from '@mui/icons-material/EventNote';
+import TransactionIcon from '../assets/icon/transaction.svg';
+import StackedLineChartIcon from '@mui/icons-material/StackedLineChart';
+import SupportAgentIcon from '@mui/icons-material/SupportAgent';
+import ForumIcon from '@mui/icons-material/Forum';
+import PersonIcon from '@mui/icons-material/Person';
+import LogoutIcon from '@mui/icons-material/Logout';
+import logo from "../assets/img/logo_admin.png"
 
-const Sidebar = () => {
-  return (
-    <div className="bg-blue-800 text-white h-screen w-[353px] fixed z-20 top-0 left-0 overflow-y-auto flex-col">
-      <div className="p-4 flex items-center justify-center">
-        <div className="p-4 flex items-center justify-center">
-          <img
-            src="/src/assets/Logotype.svg"
-            alt="Logo LokaSani"
-            role="presentation"
-            className="w-8 h-8 mr-2"
-          />
-          <h1 className="text-2xl font-bold  flex justify-center items-center">
-            ADMIN UMKM
-          </h1>
-        </div>
-      </div>
-      <div className="flex flex-col items-center justify-center">
-        <button className="flex items-center justify-center bg-gray-300 text-white h-10 px-4 rounded-md mb-4 mt-10">
-          <span className="font-medium">Tambahkan Produk</span>
+function Sidebar() {
+    const navigate = useNavigate();
+    const [sidebar, setSidebar] = useState(false);
+    const openSidebar = () => {
+        setSidebar(!sidebar);
+    };
+
+    return (
+    <>
+        <button onClick={openSidebar} type="button" className="inline-flex items-center p-2 mt-2 ml-5 text-sm text-white rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 hover:bg-gray-700 focus:ring-gray-600">
+            <span className="sr-only">Open sidebar</span>
+            <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                <path clipRule='evenodd' fillRule='evenodd' d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
+            </svg>
         </button>
-      </div>
 
-      <ul className=" flex-col items-center justify-center">
-        <li className="flex items-center h-10 px-4 font-medium text-sm justify-start">
-          <MdHome className="text-2xl mr-2" />
-          <span className="leading-none">Home</span>
-        </li>
-        <li className="flex items-center h-10 px-4 font-medium text-sm justify-start">
-          <MdChat className="text-2xl mr-2" />
-          <span className="leading-none">Chat</span>
-        </li>
-        <li className="flex items-center h-10 px-4 font-medium text-sm justify-start">
-          <MdShoppingCart className="text-2xl mr-2" />
-          <span className="leading-none">Produk</span>
-        </li>
-        <li className="flex items-center h-10 px-4 font-medium text-sm justify-start">
-          <MdReceipt className="text-2xl mr-2" />
-          <span className="leading-none">Pesanan</span>
-        </li>
-        <li className="flex items-center h-10 px-4 font-medium text-sm justify-start">
-          <MdConveyorBelt className="text-2xl mr-2" />
-          <span className="leading-none">Transaksi</span>
-        </li>
-        <li className="flex items-center h-10 px-4 font-medium text-sm justify-start">
-          <MdQuestionAnswer className="text-2xl mr-2" />
-          <span className="leading-none">Statistik</span>
-        </li>
-        <li className="flex items-center h-10 px-4 font-medium text-sm justify-start">
-          <MdGroup className="text-2xl mr-2" />
-          <span className="leading-none">Pusat Bantuan</span>
-        </li>
-        <li className="flex items-center h-10 px-4 font-medium text-sm justify-start">
-          <MdQuestionAnswer className="text-2xl mr-2" />
-          <span className="leading-none">Ulasan</span>
-        </li>
-        <li className="flex items-center h-10 px-4 font-medium text-sm justify-start">
-          <MdGroup className="text-2xl mr-2" />
-          <span className="leading-none">Setting</span>
-        </li>
-        <li className="flex items-center h-10 px-4 font-medium text-sm justify-start">
-          <MdLogout className="text-2xl mr-2" />
-          <span className="leading-none">logout</span>
-        </li>
-      </ul>
-    </div>
-  );
-};
+        <aside 
+            className={`fixed top-0 left-0 z-40 w-64 h-screen transition-transform ${
+                sidebar ? 'translate-x-0' : '-translate-x-full'
+              } sm:translate-x-0`}
+            id="logo-sidebar">
+            <div className="h-screen px-3 py-4 overflow-y-auto bg-[#253E8D]">
+                <h2 href="https://flowbite.com/" className="flex items-center pl-2.5 mb-9 mt-6">
+                    <img src={logo} className="h-6 mr-3 sm:h-7" alt="Flowbite Logo" />
+                    <span className="custom-font font-medium pt-2 text-white">CREATOR.</span>
+                    <svg
+                    className="block h-6 w-6 ml-20 text-white cursor-pointer hover:text-blue-500 md:hidden"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    aria-hidden="true"
+                    onClick={openSidebar}
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  </svg>
+                </h2>
 
-export default Sidebar;
+                <div className='mx-2 mb-10'>
+                    <a className='cursor-pointer flex items-center justify-center py-3 p-2 text-gray-900 rounded-full text-white bg-[#768DD5]'>+ Add New Event</a>
+                </div>
+                
+                <div className='flex flex-col space-y-[15vh]'>
+                    <div className='mx-3'>
+                        <ul className="space-y-3">
+                            <li>
+                                <a
+                                className="cursor-pointer text-sm flex items-center px-2 py-1 text-white rounded-sm hover:bg-gray-700 group">
+                                <HomeIcon fontSize='small'/>
+                                <span className="flex-1 ml-3 whitespace-nowrap ">Home</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a
+                                className="cursor-pointer text-sm  flex items-center px-2 py-1 rounded-sm text-white hover:bg-gray-700 group">
+                                <ChatIcon fontSize='small'/>
+                                <span className="flex-1 ml-3 whitespace-nowrap">Chat</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a
+                                className="cursor-pointer text-sm  flex items-center px-2 py-1 rounded-sm text-white hover:bg-gray-700 group">
+                                <ShoppingCartIcon fontSize='small'/>
+                                <span className="flex-1 ml-3 whitespace-nowrap">Produk</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a
+                                className="cursor-pointer text-sm  flex items-center px-2 py-1 rounded-sm text-white hover:bg-gray-700 group">
+                                <EventNoteIcon fontSize='small'/>
+                                <span className="flex-1 ml-3 whitespace-nowrap">Pesanan</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a
+                                className="cursor-pointer text-sm  flex items-center px-2 py-1 rounded-sm text-white hover:bg-gray-700 group">
+                                <img src={TransactionIcon} alt="Transaction" className='w-5 h-5'/>
+                                <span className="flex-1 ml-3 whitespace-nowrap">Transaksi</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a
+                                className="cursor-pointer text-sm  flex items-center px-2 py-1 rounded-sm text-white hover:bg-gray-700 group">
+                                <StackedLineChartIcon fontSize='small'/>
+                                <span className="flex-1 ml-3 whitespace-nowrap">Statistik</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a
+                                className="cursor-pointer text-sm  flex items-center px-2 py-1 rounded-sm text-white hover:bg-gray-700 group">
+                                <SupportAgentIcon fontSize='small'/>
+                                <span className="flex-1 ml-3 whitespace-nowrap">Pusat Bantuan</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a
+                                className="cursor-pointer text-sm  flex items-center px-2 py-1 rounded-sm text-white hover:bg-gray-700 group">
+                                <ForumIcon fontSize='small'/>
+                                <span className="flex-1 ml-3 whitespace-nowrap">Ulasan</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div className='mx-3 '>
+                        <ul className="space-y-3">
+                            <li>
+                                <a
+                                className="cursor-pointer text-sm  flex items-center px-2 py-1 rounded-sm text-white hover:bg-gray-700 group">
+                                <PersonIcon fontSize='small'/>
+                                <span className="flex-1 ml-3 whitespace-nowrap">Akun</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a
+                                className="cursor-pointer text-sm  flex items-center px-2 py-1 rounded-sm text-white hover:bg-gray-700 group">
+                                <LogoutIcon fontSize='small'/>
+                                <span className="flex-1 ml-3 whitespace-nowrap">Logout</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </aside>
+    </>
+  )
+}
+
+export default Sidebar
