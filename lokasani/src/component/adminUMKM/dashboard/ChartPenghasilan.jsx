0a0859c8c96162df577cf6b8227d416bd5c9a57a
faurrahman10 <faturrahmanhasir@gmail.com>
2023-11-23 19:24:53 +0800

    mengubah warna dan ukuran line pada chart

--- lokasani/src/component/adminUMKM/dashboard/ChartPenghasilan.jsx
+++ lokasani/src/component/adminUMKM/dashboard/ChartPenghasilan.jsx
@@ -8,15 +8,15 @@ const ChartPenghasilan = () => {
                 label: "Pembayaran",
                 data: [1000, 1500, 1200, 1700, 1300, 1600, 1100, 1900, 1400, 1800, 2000, 1700],
                 fill: false,
-                borderColor: "rgba(54, 162, 235, 1)",
-                borderWidth: 1,
+                borderColor: "#3377FF",
+                borderWidth: 2,
             },
             {
                 label: "Pengeluaran",
                 data: [800, 1200, 900, 1500, 1000, 1300, 850, 1600, 1100, 1400, 1500, 1300],
                 fill: false,
-                borderColor: "rgba(255, 99, 132, 1)",
-                borderWidth: 1,
+                borderColor: "#E8644B",
+                borderWidth: 2,
             },
         ],
     };
@@ -38,8 +38,8 @@ const ChartPenghasilan = () => {
 
     return (
         <div className="w-full rounded-lg bg-white p-6">
-            <h2>Penghasilan</h2>
-            <Line height={188} data={data} options={options} />
+            <h2 className="text-xl font-bold">Penghasilan</h2>
+            <Line height={185} data={data} options={options} />
         </div>
     );
 };
--- lokasani/src/component/adminUMKM/dashboard/GrafikBulanan.jsx
+++ lokasani/src/component/adminUMKM/dashboard/GrafikBulanan.jsx
@@ -2,17 +2,20 @@ import { Line } from "react-chartjs-2";
 import Chart from "chart.js/auto";
 
 const data = {
-  labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug"],
+  labels: Array.from({ length: 30 }, (_, i) => (i + 1).toString()),
   datasets: [
     {
       label: "Penjualan Bulanan",
       backgroundColor: "rgba(75,192,192,1)",
-      borderColor: "rgba(0,0,0,1)",
+      borderColor: "#3653B0",
       borderWidth: 2,
-      data: [20, 30, 60, 81, 56, 55, 90, 20],
+      data: [
+        20, 30, 60, 81, 56, 55, 90, 20, 35, 45, 60, 70, 80, 30, 45, 55, 60, 70, 75, 85, 90, 95, 80, 75, 70, 60, 50, 40, 30
+      ],
     },
   ],
 };
+
 
 const options = {
   scales: {
