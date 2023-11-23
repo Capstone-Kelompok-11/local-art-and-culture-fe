d883815e53334aea6566111ea34d9fca00a088b2
faurrahman10 <faturrahmanhasir@gmail.com>
2023-11-23 11:30:59 +0800

    mengubah endpoint route

--- lokasani/src/App.jsx
+++ lokasani/src/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
+import Home from "./pages/dashboard-admin-umkm";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/adminumkm" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
