55ab4d264fa7bbd87153b4f3d6823489286843db
faurrahman10 <faturrahmanhasir@gmail.com>
2023-11-23 12:14:29 +0800

    ubah struktur folder pages

--- lokasani/src/App.jsx
+++ lokasani/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./pages/dashboard-admin-umkm";
+import Home from "./pages/adminUMKM/Home";
 
 function App() {
   return (
