7924fc3ee7a3e2f77775e42551e50462fdc2da11
faurrahman10 <faturrahmanhasir@gmail.com>
2023-11-25 23:34:08 +0800

    menambahkan component baru ke dalam page product

--- lokasani/src/pages/adminUMKM/Product.jsx
+++ lokasani/src/pages/adminUMKM/Product.jsx
@@ -2,8 +2,12 @@ import Sidebar from "../../component/adminUMKM/globalComponent/Sidebar";
 import Header from "../../component/adminUMKM/globalComponent/Header";
 import InformasiProduct from "../../component/adminUMKM/product/InformasiProduct";
 import Harga from "../../component/adminUMKM/product/Harga";
+import Varian from "../../component/adminUMKM/product/Varian";
+import Mengatur from "../../component/adminUMKM/product/Mengatur";
+import Media from "../../component/adminUMKM/product/Media";
 
 const Product = () => {
+
   return (
     <div className="bg-[#F2F2F2]">
       <div className="flex">
@@ -14,31 +18,47 @@ const Product = () => {
           <div>
             <Header headerTitle="Tambah Produk" />
           </div>
-          <div className="w-full flex justify-between items-center">
-            <div>
-              <h1 className="text-xl ml-10 mt-10 font-bold">
+          <div className="w-full flex justify-between items-center px-6 mt-8">
+            <div className="w-full">
+              <h1 className="text-xl font-bold">
                 Tambahkan Product
               </h1>
-              <p className="text-sm ml-10">
+              <p className="text-sm">
                 Pesanan ditempatkan diseluruh toko anda
               </p>
             </div>
-            <div className="flex justify-end items-center w-full">
-              <button className=" border border-slate-200 bg-blue-800 text-white rounded-md  p-3 ">
+            <div className="flex justify-end items-center gap-3 w-full">
+              <button className=" border border-slate-200 bg-[#253E8D] text-white rounded-md py-2 px-3 ">
                 Simpan
               </button>
-              <button className=" border border-slate-200 bg-blue-800 text-white rounded-md  p-3 ml-4 ">
+              <button className=" border border-slate-200 bg-[#253E8D] text-white rounded-md py-2 px-3">
                 Batalkan
               </button>
-              <button className=" border border-slate-200 bg-blue-800 text-white rounded-md  p-3 ml-4 ">
+              <button className=" border border-slate-200 bg-[#253E8D] text-white rounded-md py-2 px-3">
                 Unggah Produk
               </button>
             </div>
           </div>
-
-          <div className="inline-flex">
-            <InformasiProduct />
-            <Harga />
+          <div className="px-6 w-full flex gap-6">
+            <div className="w-3/5">
+              <div className="w-full">
+                <InformasiProduct />
+              </div>
+              <div className="w-full">
+                <Media/>
+              </div>
+              <div className="w-full">
+                <Varian/>
+              </div>
+            </div>
+            <div className="w-2/5">
+              <div className="w-full">
+                <Harga />
+              </div>
+              <div>
+                <Mengatur/>
+              </div>
+            </div>
           </div>
         </div>
       </div>
