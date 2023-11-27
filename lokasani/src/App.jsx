aa92926d92bbb1184dcf8e1954a70f65aed19847
Hastinul Chotimah <142318032+hastinulchotimah@users.noreply.github.com>
2023-11-27 12:49:43 +0700

    add new event

--- lokasani/src/App.jsx
+++ lokasani/src/App.jsx
@@ -1,6 +1,9 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import AdminEvent from './pages/adminEvent/Index'
 import Home from "./pages/home"
+import NewEvent from './pages/adminEvent/newevent'
+
+
 
 function App() {
   return (
@@ -8,6 +11,7 @@ function App() {
       <Routes>
         <Route element={<Home/>} path='/'/>
         <Route element={<AdminEvent/>} path='/adminevent'/>
+        <Route element={<NewEvent/>} path='/newevent'/>
       </Routes>
     </BrowserRouter>
   )
