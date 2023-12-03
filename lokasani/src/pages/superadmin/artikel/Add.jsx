138eb54c98d2092b9fb7e2e36bec1d27abd1f0ff
Saitampool <dimashasim1106@gmail.com>
2023-12-03 08:43:23 +0700

    completing add artikel

--- lokasani/src/pages/superadmin/artikel/Add.jsx
+++ lokasani/src/pages/superadmin/artikel/Add.jsx
@@ -1,34 +1,218 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
-import Sidebar from '../../../component/superadmin/globalComponent/Sidebar';
-import Navbar from '../../../component/superadmin/artikel/Navbar';
-import AddPhoto from '../../../assets/icon/library_add.svg'
-import AddIcon from '@mui/icons-material/Add';
+import React, { useState } from "react";
+import Sidebar from "../../../component/superadmin/globalComponent/Sidebar";
+import Navbar from "../../../component/superadmin/artikel/Navbar";
+import AddIcon from "@mui/icons-material/Add";
+import AddAPhotoIcon from "@mui/icons-material/AddAPhoto";
+import imageSearching from "../../../assets/img/image-searching.png";
 
 function Add() {
+  // function thumbnail
+  const [thumbnail, setThumbnail] = useState(null);
+  const [thumbnailPreview, setThumbnailPreview] = useState(null);
+  const [isPopupThumbnailOpen, setIsPopupThumbnailOpen] = useState(false);
+
+  const handleThumbnailUpload = (event) => {
+    setIsPopupThumbnailOpen(false);
+    console.log(thumbnail);
+  };
+  const handlePopupUploadThumbnail = () => {
+    setIsPopupThumbnailOpen(true);
+  };
+  const handleFileThumbnailChange = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        setThumbnailPreview(reader.result);
+      };
+      reader.readAsDataURL(file);
+    }
+    setThumbnail(file);
+  };
+  const handleDragOverThumbnail = (event) => {
+    event.preventDefault();
+  };
+  const handleDropThumbnail = (event) => {
+    event.preventDefault();
+    const file = event.dataTransfer.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        setThumbnailPreview(reader.result);
+      };
+      reader.readAsDataURL(file);
+    }
+    setThumbnail(file);
+  };
+  const handleThumbnailClick = () => {
+    document.getElementById("uploadThumbnail").click();
+  };
+
   return (
     <section>
-        <Sidebar />
-        <Navbar title="Tulis Artikel" />
-        <main className='pt-[100px] h-full sm:ml-[249px] bg-[#D9D9D9]'>
-            <div className='mx-5 bg-white rounded-md mt-2 px-6 pt-10 pb-3'>
-                <h1 className='font-semibold text-2xl'>Unggah Tumbnail</h1>
-                <div className='mt-3 flex gap-x-3 mb-4'>
-                    <div className='h-[160px] w-[320px] rounded-md flex justify-center items-center bg-[#D9D9D9]'>
-                        <img src={AddPhoto} alt="" className='w-[80px] h-[80px] bg-[#D9D9D9]'/>
-                    </div>
-                    <div>
-                        <span>Ukuran file maks 10MB</span><br />
-                        <button className='bg-[#768DD5] hover:bg-[#91A3DD] rounded-[20px] text-white py-2 px-5 text-xs mt-1'><AddIcon fontSize='small'/> &nbsp; Unggah gambar</button>
-                    </div>
-                </div>
+      <Sidebar />
+      <Navbar title="Tulis Artikel" />
+      <main className="pt-[100px] h-full sm:ml-[249px] bg-[#D9D9D9]">
+        <div className="mx-5 bg-white rounded-md mt-2 px-6 pt-6 pb-3">
+          <div className="flex gap-x-3 mb-4 w-full">
+            <div className="h-[250px] w-[500px] rounded-[15px] flex flex-col justify-center items-center bg-white outline-dashed outline-1 outline-[#828282]">
+              <div className="bg-[#768DD5] w-10 h-10 flex justify-center items-center rounded-md">
+                <AddAPhotoIcon className="text-white" />
+              </div>
+              <label className="text-[#768DD5] mt-3">
+                Unggah thumbnail artikel di sini*
+              </label>
+              <input
+                type="file"
+                accept="image/*"
+                id="thumbnail"
+                onChange={handleThumbnailUpload}
+                className="hidden"
+              />
+              <p className="text-[#828282] text-sm mb-2">
+                *Upload gambar untuk artikel Max ukuran 500kb
+              </p>
+              {thumbnail && (
+                <span className="ml-2 text-gray-500 mb-2">
+                  {thumbnail.name}
+                </span>
+              )}
+              <button
+                onClick={handlePopupUploadThumbnail}
+                className="bg-[#3653B0] text-white py-2 px-3 rounded-full hover:bg-[#768DD5]"
+              >
+                <AddIcon /> Unggah Gambar
+              </button>
+              {/* popup poster start */}
+              {isPopupThumbnailOpen && (
                 <div>
-                    <hr className='h-[1px] bg-[#CCCCCC]'/>
+                  <div className="fixed inset-0 flex items-center justify-center z-40">
+                    <div
+                      className="absolute inset-0 bg-gray-800 opacity-50"
+                      onClick={() => setIsPopupThumbnailOpen(false)}
+                    ></div>
+                    <div
+                      className="w-5/12 bg-white p-4 rounded-lg z-50"
+                      onDragOver={handleDragOverThumbnail}
+                      onDrop={handleDropThumbnail}
+                    >
+                      <h1 className="text-xl font-semibold mb-3 pb-2 border-b-2">
+                        Unggah Gambar
+                      </h1>
+                      <div
+                        className="rounded-lg text-center"
+                        onClick={handleThumbnailClick}
+                      >
+                        {thumbnailPreview ? (
+                          <img
+                            src={thumbnailPreview}
+                            alt="Preview"
+                            className="w-60 h-52 mx-auto cursor-pointer"
+                          />
+                        ) : (
+                          <img
+                            src={imageSearching}
+                            alt="Searching"
+                            className="w-60 h-52 mx-auto cursor-pointer"
+                          />
+                        )}
+                      </div>
+                      <div className="flex justify-center flex-col items-center my-3">
+                        <label
+                          htmlFor="uploadThumbnail"
+                          className="font-bold cursor-pointer text-[24px]"
+                        >
+                          Tarik dan Lepaskan Gambar
+                        </label>
+                        <p className="text-[14px] text-[#828282]">
+                          *Unggah gambar untuk poster event kamu Max ukuran
+                          500kb
+                        </p>
+                        <input
+                          type="file"
+                          id="uploadThumbnail"
+                          className="hidden"
+                          accept="image/*"
+                          onChange={handleFileThumbnailChange}
+                        />
+                      </div>
+                      <div>
+                        <div className="mt-4 flex justify-center">
+                          <button
+                            className="w-5/12 px-4 py-2 rounded-[20px] focus:outline-none text-white bg-[#253E8D]"
+                            onClick={() => handleThumbnailUpload()}
+                          >
+                            Tambahkan
+                          </button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
                 </div>
+              )}
+              {/* popup poster end */}
             </div>
-        </main>
+            <div className="ml-7 w-1/2">
+              <div>
+                <label
+                  htmlFor="judul"
+                  className="text-[#768DD5] font-semibold text-[20px]"
+                >
+                  Judul Artikel
+                </label>
+                <br />
+                <input
+                  type="text"
+                  id="judul"
+                  placeholder="Judul..."
+                  className="bg-[#F2F2F2] rounded-[12px] py-2 px-4 w-full mt-1 mb-5"
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="penulis"
+                  className="text-[#768DD5] font-semibold text-[20px]"
+                >
+                  Penulis Artikel
+                </label>
+                <br />
+                <input
+                  type="text"
+                  id="penulis"
+                  placeholder="Penulis..."
+                  className="bg-[#F2F2F2] rounded-[12px] py-2 px-4 w-full mt-1 mb-5"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="px-6 pt-6 pb-4">
+          <textarea
+            name=""
+            id=""
+            cols="30"
+            rows="16"
+            className="w-full rounded-md py-4 px-3 resize-none"
+            placeholder="Tulis isi artikel di sini"
+          />
+          <div className="flex justify-between mt-3">
+            <button className="bg-[#E8644B] text-white px-10 rounded-full py-1">
+              Batal
+            </button>
+            <div>
+              <button className="bg-[#F3B502] text-white px-7 rounded-full py-1 mr-6">
+                Simpan Draf
+              </button>
+              <button className="bg-[#3653B0] text-white px-10 rounded-full py-1">
+                Unggah
+              </button>
+            </div>
+          </div>
+        </div>
+      </main>
     </section>
-  )
+  );
 }
 
-export default Add
+export default Add;
--- lokasani/src/pages/superadmin/artikel/index.jsx
+++ lokasani/src/pages/superadmin/artikel/index.jsx
@@ -1,135 +1,189 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 /* eslint-disable no-unused-vars */
-import React from 'react'
-import {useNavigate} from 'react-router-dom'
-import Sidebar from '../../../component/superadmin/globalComponent/Sidebar';
-import Navbar from '../../../component/superadmin/globalComponent/Navbar';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import TextsmsIcon from '@mui/icons-material/Textsms';
-import VisibilityIcon from '@mui/icons-material/Visibility';
-import QuestionAnswerIcon from '@mui/icons-material/QuestionAnswer';
-import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
-import EditIcon from '@mui/icons-material/Edit';
-import PlaceImage from '../../../assets/img/placeimage-artikel.png'
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import Sidebar from "../../../component/superadmin/globalComponent/Sidebar";
+import Navbar from "../../../component/superadmin/globalComponent/Navbar";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import TextsmsIcon from "@mui/icons-material/Textsms";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import QuestionAnswerIcon from "@mui/icons-material/QuestionAnswer";
+import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
+import EditIcon from "@mui/icons-material/Edit";
+import PlaceImage from "../../../assets/img/placeimage-artikel.png";
 
 function index() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
   return (
     <section>
-        <Sidebar />
-        <Navbar title="Artikel" />
+      <Sidebar />
+      <Navbar title="Artikel" />
 
-        {/* content start */}
-        <div className='pt-[114px] h-screen sm:ml-[316px]'>
-            <div className='flex flex-row justify-center items-center gap-x-10 px-4'>
-                <div className=' w-52 h-[110px] flex flex-col gap-y-3 '>
-                    <div 
-                    onClick={() => {
-                        navigate('/superadmin/addartikel')
-                    }}
-                    className='bg-[#F3B502] hover:bg-amber-500 rounded-[16px] h-[50px] flex items-center justify-center gap-x-2 cursor-pointer'><EditIcon fontSize='small' className='text-white'/><span className='text-white text-sm font-medium'>Tulis Artikel</span></div>
-                    <div
-                        onClick={() => {
-                            navigate('/superadmin/kelolaartikel')
-                        }}
-                     className='bg-[#3653B0] hover:bg-blue-900 rounded-[16px] h-[50px] flex items-center justify-center cursor-pointer'><span className='text-white text-sm font-medium'>Kelola Artikel</span></div>
-                </div>
-                <div className='bg-slate-200 w-48 h-[110px] rounded-[16px] pl-3 pt-2'>
-                    <span className=''>Feedback</span><br />
-                    <div className='flex ml-3'>
-                        <FavoriteIcon fontSize='large' className='text-[#FF3B3B] mt-4'/>
-                        <div className='font-semibold text-[40px] mt-1 ml-3'>76</div>
-                    </div>
-                </div>
-                <div className='bg-slate-200 w-48 h-[110px] rounded-[16px] pl-3 pt-2'>
-                    <span>Pembaca</span><br />
-                    <div className='flex ml-3'>
-                        <VisibilityIcon fontSize='large' className='text-[#339981] mt-4'/>
-                        <div className='font-semibold text-[40px] mt-1 ml-3'>76</div>
-                    </div>
-                </div>
-                <div className='bg-slate-200 w-48 h-[110px] rounded-[16px] pl-3 pt-2'>
-                    <span>Laporan</span><KeyboardArrowRightIcon fontSize='small' className='bg-blue-500 cursor-pointer hover:bg-blue-600 rounded-full ml-2 text-white'/><br />
-                    <div className='flex ml-3'>
-                        <QuestionAnswerIcon fontSize='large' className='text-[#FF3B3B] mt-4'/>
-                        <div className='font-semibold text-[40px] mt-1 ml-3'>76</div>
-                    </div>
-                </div>
+      {/* content start */}
+      <div className="pt-[114px] h-screen sm:ml-[316px]">
+        <div className="flex flex-row justify-center items-center gap-x-10 px-4">
+          <div className=" w-52 h-[110px] flex flex-col gap-y-3 ">
+            <div
+              onClick={() => {
+                navigate("/superadmin/addartikel");
+              }}
+              className="bg-[#F3B502] hover:bg-amber-500 rounded-[16px] h-[50px] flex items-center justify-center gap-x-2 cursor-pointer"
+            >
+              <EditIcon fontSize="small" className="text-white" />
+              <span className="text-white text-sm font-medium">
+                Tulis Artikel
+              </span>
             </div>
-
-            <div className='mt-6 mx-auto pl-6 w-[95%] bg-slate-200 rounded-[10px] pb-4 mb-3'>
-                <div><h1 className='pt-3 font-semibold text-2xl'>Artikel terbaru</h1></div>
-                <div className='pt-2 flex flex-wrap justify-start gap-x-20 px-3 gap-y-5'>
-                    <div className='w-[250px]'>
-                        <img src={PlaceImage} width={250} alt="" />
-                        <h1 className='font-semibold text-sm mt-2 ml-1'>Memaknai Wayang sebagai Khazanah Budaya Nusantara Masa Kini</h1>
-                        <div className='ml-1 mt-3 flex items-center text-[#999999]'>
-                            <VisibilityIcon fontSize='small'/>&nbsp;<span>12</span>
-                            <TextsmsIcon fontSize='small' className='ml-3'/>&nbsp;<span>8</span>
-                            <h1 className='ml-6 text-sm'>12 November 2023</h1>
-                        </div>
-                    </div>
-                    <div className='w-[250px]'>
-                        <img src={PlaceImage} width={250} alt="" />
-                        <h1 className='font-semibold text-sm mt-2 ml-1'>Memaknai Wayang sebagai Khazanah Budaya Nusantara Masa Kini</h1>
-                        <div className='ml-1 mt-3 flex items-center text-[#999999]'>
-                            <VisibilityIcon fontSize='small'/>&nbsp;<span>12</span>
-                            <TextsmsIcon fontSize='small' className='ml-3'/>&nbsp;<span>8</span>
-                            <h1 className='ml-6 text-sm'>12 November 2023</h1>
-                        </div>
-                    </div>
-                    <div className='w-[250px]'>
-                        <img src={PlaceImage} width={250} alt="" />
-                        <h1 className='font-semibold text-sm mt-2 ml-1'>Memaknai Wayang sebagai Khazanah Budaya Nusantara Masa Kini</h1>
-                        <div className='ml-1 mt-3 flex items-center text-[#999999]'>
-                            <VisibilityIcon fontSize='small'/>&nbsp;<span>12</span>
-                            <TextsmsIcon fontSize='small' className='ml-3'/>&nbsp;<span>8</span>
-                            <h1 className='ml-6 text-sm'>12 November 2023</h1>
-                        </div>
-                    </div>
-                    <div className='w-[250px]'>
-                        <img src={PlaceImage} width={250} alt="" />
-                        <h1 className='font-semibold text-sm mt-2 ml-1'>Memaknai Wayang sebagai Khazanah Budaya Nusantara Masa Kini</h1>
-                        <div className='ml-1 mt-3 flex items-center text-[#999999]'>
-                            <VisibilityIcon fontSize='small'/>&nbsp;<span>12</span>
-                            <TextsmsIcon fontSize='small' className='ml-3'/>&nbsp;<span>8</span>
-                            <h1 className='ml-6 text-sm'>12 November 2023</h1>
-                        </div>
-                    </div>
-                    <div className='w-[250px]'>
-                        <img src={PlaceImage} width={250} alt="" />
-                        <h1 className='font-semibold text-sm mt-2 ml-1'>Memaknai Wayang sebagai Khazanah Budaya Nusantara Masa Kini</h1>
-                        <div className='ml-1 mt-3 flex items-center text-[#999999]'>
-                            <VisibilityIcon fontSize='small'/>&nbsp;<span>12</span>
-                            <TextsmsIcon fontSize='small' className='ml-3'/>&nbsp;<span>8</span>
-                            <h1 className='ml-6 text-sm'>12 November 2023</h1>
-                        </div>
-                    </div>
-                    <div className='w-[250px]'>
-                        <img src={PlaceImage} width={250} alt="" />
-                        <h1 className='font-semibold text-sm mt-2 ml-1'>Memaknai Wayang sebagai Khazanah Budaya Nusantara Masa Kini</h1>
-                        <div className='ml-1 mt-3 flex items-center text-[#999999]'>
-                            <VisibilityIcon fontSize='small'/>&nbsp;<span>12</span>
-                            <TextsmsIcon fontSize='small' className='ml-3'/>&nbsp;<span>8</span>
-                            <h1 className='ml-6 text-sm'>12 November 2023</h1>
-                        </div>
-                    </div>
-                    <div className='w-[250px]'>
-                        <img src={PlaceImage} width={250} alt="" />
-                        <h1 className='font-semibold text-sm mt-2 ml-1'>Memaknai Wayang sebagai Khazanah Budaya Nusantara Masa Kini</h1>
-                        <div className='ml-1 mt-3 flex items-center text-[#999999]'>
-                            <VisibilityIcon fontSize='small'/>&nbsp;<span>12</span>
-                            <TextsmsIcon fontSize='small' className='ml-3'/>&nbsp;<span>8</span>
-                            <h1 className='ml-6 text-sm'>12 November 2023</h1>
-                        </div>
-                    </div>
-                </div>
+            <div
+              onClick={() => {
+                navigate("/superadmin/kelolaartikel");
+              }}
+              className="bg-[#3653B0] hover:bg-blue-900 rounded-[16px] h-[50px] flex items-center justify-center cursor-pointer"
+            >
+              <span className="text-white text-sm font-medium">
+                Kelola Artikel
+              </span>
             </div>
+          </div>
+          <div className="bg-slate-200 w-48 h-[110px] rounded-[16px] pl-3 pt-2">
+            <span className="">Feedback</span>
+            <br />
+            <div className="flex ml-3">
+              <FavoriteIcon fontSize="large" className="text-[#FF3B3B] mt-4" />
+              <div className="font-semibold text-[40px] mt-1 ml-3">76</div>
+            </div>
+          </div>
+          <div className="bg-slate-200 w-48 h-[110px] rounded-[16px] pl-3 pt-2">
+            <span>Pembaca</span>
+            <br />
+            <div className="flex ml-3">
+              <VisibilityIcon
+                fontSize="large"
+                className="text-[#339981] mt-4"
+              />
+              <div className="font-semibold text-[40px] mt-1 ml-3">76</div>
+            </div>
+          </div>
+          <div className="bg-slate-200 w-48 h-[110px] rounded-[16px] pl-3 pt-2">
+            <span>Laporan</span>
+            <KeyboardArrowRightIcon
+              fontSize="small"
+              className="bg-blue-500 cursor-pointer hover:bg-blue-600 rounded-full ml-2 text-white"
+            />
+            <br />
+            <div className="flex ml-3">
+              <QuestionAnswerIcon
+                fontSize="large"
+                className="text-[#FF3B3B] mt-4"
+              />
+              <div className="font-semibold text-[40px] mt-1 ml-3">76</div>
+            </div>
+          </div>
         </div>
-        
-        {/* content end */}
+
+        <div className="mt-6 mx-auto pl-6 w-[95%] bg-slate-200 rounded-[10px] pb-4 mb-3">
+          <div>
+            <h1 className="pt-3 font-semibold text-2xl">Artikel terbaru</h1>
+          </div>
+          <div className="pt-2 grid grid-cols-3 2xl:grid-cols-4 gap-y-5 px-auto">
+            <div className="w-[250px]">
+              <img src={PlaceImage} width={250} alt="" />
+              <h1 className="font-semibold text-sm mt-2 ml-1">
+                Memaknai Wayang sebagai Khazanah Budaya Nusantara Masa Kini
+              </h1>
+              <div className="ml-1 mt-3 flex items-center text-[#999999]">
+                <VisibilityIcon fontSize="small" />
+                &nbsp;<span>12</span>
+                <TextsmsIcon fontSize="small" className="ml-3" />
+                &nbsp;<span>8</span>
+                <h1 className="ml-6 text-sm">12 November 2023</h1>
+              </div>
+            </div>
+            <div className="w-[250px]">
+              <img src={PlaceImage} width={250} alt="" />
+              <h1 className="font-semibold text-sm mt-2 ml-1">
+                Memaknai Wayang sebagai Khazanah Budaya Nusantara Masa Kini
+              </h1>
+              <div className="ml-1 mt-3 flex items-center text-[#999999]">
+                <VisibilityIcon fontSize="small" />
+                &nbsp;<span>12</span>
+                <TextsmsIcon fontSize="small" className="ml-3" />
+                &nbsp;<span>8</span>
+                <h1 className="ml-6 text-sm">12 November 2023</h1>
+              </div>
+            </div>
+            <div className="w-[250px]">
+              <img src={PlaceImage} width={250} alt="" />
+              <h1 className="font-semibold text-sm mt-2 ml-1">
+                Memaknai Wayang sebagai Khazanah Budaya Nusantara Masa Kini
+              </h1>
+              <div className="ml-1 mt-3 flex items-center text-[#999999]">
+                <VisibilityIcon fontSize="small" />
+                &nbsp;<span>12</span>
+                <TextsmsIcon fontSize="small" className="ml-3" />
+                &nbsp;<span>8</span>
+                <h1 className="ml-6 text-sm">12 November 2023</h1>
+              </div>
+            </div>
+            <div className="w-[250px]">
+              <img src={PlaceImage} width={250} alt="" />
+              <h1 className="font-semibold text-sm mt-2 ml-1">
+                Memaknai Wayang sebagai Khazanah Budaya Nusantara Masa Kini
+              </h1>
+              <div className="ml-1 mt-3 flex items-center text-[#999999]">
+                <VisibilityIcon fontSize="small" />
+                &nbsp;<span>12</span>
+                <TextsmsIcon fontSize="small" className="ml-3" />
+                &nbsp;<span>8</span>
+                <h1 className="ml-6 text-sm">12 November 2023</h1>
+              </div>
+            </div>
+            <div className="w-[250px]">
+              <img src={PlaceImage} width={250} alt="" />
+              <h1 className="font-semibold text-sm mt-2 ml-1">
+                Memaknai Wayang sebagai Khazanah Budaya Nusantara Masa Kini
+              </h1>
+              <div className="ml-1 mt-3 flex items-center text-[#999999]">
+                <VisibilityIcon fontSize="small" />
+                &nbsp;<span>12</span>
+                <TextsmsIcon fontSize="small" className="ml-3" />
+                &nbsp;<span>8</span>
+                <h1 className="ml-6 text-sm">12 November 2023</h1>
+              </div>
+            </div>
+            <div className="w-[250px]">
+              <img src={PlaceImage} width={250} alt="" />
+              <h1 className="font-semibold text-sm mt-2 ml-1">
+                Memaknai Wayang sebagai Khazanah Budaya Nusantara Masa Kini
+              </h1>
+              <div className="ml-1 mt-3 flex items-center text-[#999999]">
+                <VisibilityIcon fontSize="small" />
+                &nbsp;<span>12</span>
+                <TextsmsIcon fontSize="small" className="ml-3" />
+                &nbsp;<span>8</span>
+                <h1 className="ml-6 text-sm">12 November 2023</h1>
+              </div>
+            </div>
+            <div className="w-[250px]">
+              <img src={PlaceImage} width={250} alt="" />
+              <h1 className="font-semibold text-sm mt-2 ml-1">
+                Memaknai Wayang sebagai Khazanah Budaya Nusantara Masa Kini
+              </h1>
+              <div className="ml-1 mt-3 flex items-center text-[#999999]">
+                <VisibilityIcon fontSize="small" />
+                &nbsp;<span>12</span>
+                <TextsmsIcon fontSize="small" className="ml-3" />
+                &nbsp;<span>8</span>
+                <h1 className="ml-6 text-sm">12 November 2023</h1>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* content end */}
     </section>
-  )
+  );
 }
 
-export default index
+export default index;
