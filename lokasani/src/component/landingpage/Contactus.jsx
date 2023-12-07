b51e60f6975a43faa2fc96da1f7cca189b80d92c
LintangTamariz <lintangtamariz03@gmail.com>
2023-12-07 11:20:15 +0700

    add page contact us and footer

--- lokasani/src/component/landingpage/Contactus.jsx
+++ lokasani/src/component/landingpage/Contactus.jsx
@@ -84,38 +84,62 @@ const Contactus = () => {
           </div>
         </div>
       </div>
-      <div className="grid grid-cols-3 gap-4 h-[100px] mb-9">
+      <div className="grid grid-cols-3 gap-4 mb-10">
         <div className="bg-blue-800 rounded-[10px] ">
-        <div class="mb-12 w-full shrink-0 grow-0 basis-auto md:w-6/12 md:px-3 lg:px-6">
+          <div class="mb-2 w-full shrink-0 grow-0 basis-auto md:w-6/12 md:px-3 lg:px-6">
             <div class="flex items-start">
-              <div class="shrink-0">
-                <div class="inline-block rounded-md bg-primary-100 p-4 text-primary">
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2"
-                    stroke="currentColor" class="h-6 w-6">
-                    <path stroke-linecap="round" stroke-linejoin="round"
-                      d="M14.25 9.75v-4.5m0 4.5h4.5m-4.5 0l6-6m-3 18c-8.284 0-15-6.716-15-15V4.5A2.25 2.25 0 014.5 2.25h1.372c.516 0 .966.351 1.091.852l1.106 4.423c.11.44-.054.902-.417 1.173l-1.293.97a1.062 1.062 0 00-.38 1.21 12.035 12.035 0 007.143 7.143c.441.162.928-.004 1.21-.38l.97-1.293a1.125 1.125 0 011.173-.417l4.423 1.106c.5.125.852.575.852 1.091V19.5a2.25 2.25 0 01-2.25 2.25h-2.25z" />
-                  </svg>
+              <div class="shrink-0 flex items-center">
+                <div class="inline-block rounded-md bg-primary-100 p-4 text-primary mt-10">
+                  <img src={Email} className="" />
                 </div>
-              </div>
-              <div class="ml-6 grow">
-                <p class="mb-2 font-bold dark:text-white">
-                  Technical support
-                </p>
-                <p class="text-neutral-500 dark:text-neutral-200">
-                  support@example.com
-                </p>
-                <p class="text-neutral-500 dark:text-neutral-200">
-                  +1 234-567-89
-                </p>
+                <div class="ml-4 mt-7">
+                  <p class="font-bold text-[24px] dark:text-white">
+                    Email</p>
+                  <p class="text-neutral-500 text-[15px] dark:text-neutral-200">
+                    cs@lokasani.com
+                  </p>
+                </div>
               </div>
             </div>
           </div>
         </div>
         <div className="bg-blue-800 rounded-[10px]">
-          <img src={Call} className="h-[50px] place-items-center" alt="" />
+        <div class="mb-12 w-full shrink-0 grow-0 basis-auto md:w-6/12 md:px-3 lg:px-6">
+            <div class="flex items-start">
+              <div class="shrink-0 flex items-center">
+                <div class="inline-block rounded-md bg-primary-100 p-4 text-primary mt-10">
+                  <img src={Call} className="" />
+                </div>
+                <div class="ml-4 mt-7">
+                  <p class="font-bold text-[24px] dark:text-white">
+                    Telepon</p>
+                  <p class="text-neutral-500 text-[15px] dark:text-neutral-200">
+                    +62 812 9234 5678
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-        <div className="bg-blue-800 rounded-[10px]">
-          <img src={Location} className="h-[50px]" alt="" />
+        
+
+        <div className="bg-blue-800 rounded-[10px] ">
+        <div class="mb-12 w-full shrink-0 grow-0 basis-auto md:w-6/12 md:px-3 lg:px-6">
+            <div class="flex items-start">
+              <div class="shrink-0 flex items-center">
+                <div class="inline-block rounded-md bg-primary-100 p-4 text-primary mt-10">
+                  <img src={Location} className="" />
+                </div>
+                <div class="ml-4 mt-7">
+                  <p class="font-bold text-[24px] dark:text-white">
+                    Alamat</p>
+                  <p class="text-neutral-500 text-[15px] dark:text-neutral-200">
+                   Jl. Lokasani No.1, Jakarta Pusat
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
