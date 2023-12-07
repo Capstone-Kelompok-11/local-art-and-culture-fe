487f6c057cd3cc16351a8edd0c4c125069bba1cb
faurrahman10 <faturrahmanhasir@gmail.com>
2023-12-07 14:49:13 +0800

    menambahkan url api

--- lokasani/src/pages/login/index.jsx
+++ lokasani/src/pages/login/index.jsx
@@ -17,7 +17,7 @@ const Login = () => {
     event.preventDefault();
 
     try {
-      const response = await axios.post('https://nama-domain-anda.com/api/login', {
+      const response = await axios.post('https://ec2-13-213-65-172.ap-southeast-1.compute.amazonaws.com:8080/admin/login', {
         email: email,
         password: password
       });
