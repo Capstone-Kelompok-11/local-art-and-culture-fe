5f49afd3437589f0f0795f87d22922ed298852a5
Saitampool <dimashasim1106@gmail.com>
2023-12-09 23:23:12 +0700

    completing design

--- lokasani/src/App.jsx
+++ lokasani/src/App.jsx
@@ -1,24 +1,24 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import AdminEvent from './pages/adminEvent/Index'
-import Home from "./pages/home"
-import NewEvent from './pages/adminEvent/newevent'
-import Transaksi from './pages/adminEvent/transaksi'
-import MyEvent from './pages/adminEvent/myevent'
-import Chat from './pages/adminEvent/chat'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AdminEvent from "./pages/adminEvent/Index";
+import Home from "./pages/home";
+import NewEvent from "./pages/adminEvent/newevent";
+import Transaksi from "./pages/adminEvent/transaksi";
+import MyEvent from "./pages/adminEvent/myevent";
+import Chat from "./pages/adminEvent/chat";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route element={<Home/>} path='/'/>
-        <Route element={<AdminEvent/>} path='/adminevent'/>
-        <Route element={<NewEvent/>} path='/newevent'/>
-        <Route element ={<Transaksi/>} path='/transaksi'/>
-        <Route element={<MyEvent/>} path='/myevent'/>
-        <Route element={<Chat/>} path='chat'/>
+        <Route element={<Home />} path="/" />
+        <Route element={<AdminEvent />} path="/adminevent" />
+        <Route element={<NewEvent />} path="/newevent" />
+        <Route element={<Transaksi />} path="/transaksi" />
+        <Route element={<MyEvent />} path="/myevent" />
+        <Route element={<Chat />} path="/adminevent/chat" />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
