40e8a75e4fc561eeafd4ff7e321a1873839cab6a
LintangTamariz <lintangtamariz03@gmail.com>
2023-12-10 13:06:23 +0700

    fix error app.jsx

--- lokasani/src/App.jsx
+++ lokasani/src/App.jsx
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import AdminEvent from './pages/adminEvent/Index'
 import Home from "./pages/home"
@@ -15,19 +15,12 @@ import CompleteFormOrganisasi from './pages/register/organisasi/CompleteForm'
 import RegisterToko from './pages/register/toko/Index'
 import CompleteFormToko from './pages/register/toko/CompleteForm'
 import Pengaturan from './pages/superadmin/pengaturan/Pengaturan'
-
-=======
-import {BrowserRouter, Routes, Route} from "react-router-dom";
->>>>>>> 68cc50b23989d1375f7b4c2667451092839cf082
-
-import Home from "./pages/home";
 import Landing from "./pages/landingpage/index"
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-<<<<<<< HEAD
         <Route element={<Home/>} path='/'/>
         <Route element={<HomeSuperAdmin/>} path='/superadmin/homeSuperAdmin'/>
         <Route element={<DataUser/>} path='/superadmin/dataUser'/>
@@ -43,10 +36,8 @@ function App() {
         <Route element={<CompleteFormOrganisasi/>} path='/auth/register/organisasi/form'/>
         <Route element={<RegisterToko/>} path='/auth/register/toko'/>
         <Route element={<CompleteFormToko/>} path='/auth/register/toko/form'/>
-=======
         <Route element={<Home/>} path="/"/>
         <Route element={<Landing/>} path="/landing"/>
->>>>>>> 68cc50b23989d1375f7b4c2667451092839cf082
       </Routes>
     </BrowserRouter>
   )
