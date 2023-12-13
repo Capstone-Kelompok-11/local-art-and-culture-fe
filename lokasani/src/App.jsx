374e3a1aa76a4b15874f4c72b02cb888e7294ec1
Saitampool <dimashasim1106@gmail.com>
2023-12-13 20:21:32 +0700

    fixing name routing

--- lokasani/src/App.jsx
+++ lokasani/src/App.jsx
@@ -1,18 +1,18 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import AdminEvent from './pages/adminEvent/Index'
-import Home from "./pages/home"
-import NewEvent from './pages/adminEvent/newevent'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AdminEvent from "./pages/adminEvent/Index";
+import Home from "./pages/home";
+import NewEvent from "./pages/adminEvent/newevent";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route element={<Home/>} path='/'/>
-        <Route element={<AdminEvent/>} path='/adminevent'/>
-        <Route element={<NewEvent/>} path='/newevent'/>
+        <Route element={<Home />} path="/" />
+        <Route element={<AdminEvent />} path="/adminevent" />
+        <Route element={<NewEvent />} path="/adminevent/newevent" />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
