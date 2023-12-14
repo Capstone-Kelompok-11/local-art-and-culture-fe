d94498b01a7021d12f8f8881433088d9e01b613a
Hastinul Chotimah <hastinulc@gmail.com>
2023-12-14 11:38:03 +0700

    perbaikin app.jsx

--- lokasani/src/App.jsx
+++ lokasani/src/App.jsx
@@ -1,7 +1,6 @@
 
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import AdminEvent from './pages/adminEvent/Index'
-import Home from "./pages/home"
 import NewEvent from './pages/adminEvent/newevent'
 import DraftEvent from './pages/adminEvent/draftevent'
 
@@ -9,11 +8,6 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route element={<HomeSuperAdmin/>} path='/superadmin/homeSuperAdmin'/>
-        <Route element={<DataUser/>} path='/superadmin/dataUser'/>
-        <Route element={<DaftarAdminEvent/>} path='/superadmin/daftarAdminEvent'/>
-        <Route element={<DaftarAdminUmkm/>} path='/superadmin/daftarAdminUmkm'/>
-        <Route element={<DaftarPengguna/>} path='/superadmin/daftarPengguna'/>
         <Route element={<AdminEvent/>} path='/adminevent'/>
         <Route element={<NewEvent/>} path='/newevent'/>
         <Route element={<DraftEvent/>} path='/draft'/>
