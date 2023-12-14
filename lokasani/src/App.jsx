40c1dd0946861a9b8c70daf4a756527f37165cc5
Hastinul Chotimah <hastinulc@gmail.com>
2023-12-14 12:10:38 +0700

    slicing edit event

--- lokasani/src/App.jsx
+++ lokasani/src/App.jsx
@@ -3,6 +3,7 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import AdminEvent from './pages/adminEvent/Index'
 import NewEvent from './pages/adminEvent/newevent'
 import DraftEvent from './pages/adminEvent/draftevent'
+import EditEvent from './pages/adminEvent/editevent';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route element={<AdminEvent/>} path='/adminevent'/>
         <Route element={<NewEvent/>} path='/newevent'/>
         <Route element={<DraftEvent/>} path='/draft'/>
+        <Route element={<EditEvent/>} path='/editevent'/>
       </Routes>
     </BrowserRouter>
   );
