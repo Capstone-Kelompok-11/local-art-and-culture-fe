207517154f6c8012c3c75d425b066e3aea73eb17
Hastinul Chotimah <hastinulc@gmail.com>
2023-12-14 14:38:09 +0700

    fixing

--- lokasani/src/App.jsx
+++ lokasani/src/App.jsx
@@ -12,10 +12,10 @@ function App() {
       <Routes>
         <Route element={<Home/>} path='/'/>
         <Route element={<AdminEvent/>} path='/adminevent'/>
-        <Route element={<NewEvent/>} path='/newevent'/>
-        <Route element ={<Transaksi/>} path='/transaksi'/>
-        <Route element={<MyEvent/>} path='/myevent'/>
-        <Route element= {<Payout/>} path='/payout'/>
+        <Route element={<NewEvent/>} path='/adminevent/newevent'/>
+        <Route element ={<Transaksi/>} path='/adminevent/transaksi'/>
+        <Route element={<MyEvent/>} path='/adminevent/myevent'/>
+        <Route element= {<Payout/>} path='/adminevent/payout'/>
       </Routes>
     </BrowserRouter>
   )
