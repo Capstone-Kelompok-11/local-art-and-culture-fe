6f63bb6712602c8a93cfa6209ffe3ad79344a1e9
LintangTamariz <lintangtamariz03@gmail.com>
2023-12-14 12:48:24 +0700

    add status in table transaksi

--- lokasani/src/pages/superadmin/transaksi/index.jsx
+++ lokasani/src/pages/superadmin/transaksi/index.jsx
@@ -4,15 +4,17 @@ import Navbar from "../../../component/superadmin/globalComponent/Navbar";
 import CardTranksasi from "../../../component/superadmin/globalComponent/CardTransaksi";
 import FileDownloadIcon from "@mui/icons-material/FileDownload";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import Date from "../../../component/superadmin/transaksi/Date";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import NorthIcon from "@mui/icons-material/North";
+
 const HomeSuperAdmin = () => {
   const usersData = [
     { judul: "Pemasukan Keseluruhan (Rp)", quantity: 35440000 },
     { judul: "Pemasukan Event (Rp)", quantity: 35440000 },
     { judul: "Pemasukan UMKM (Rp)", quantity: 35440000 },
-    { judul: "Permintaan Payout", quantity: 32 },
+    // { judul: "Permintaan Payout", quantity: 32 },
   ];
-
-  
 
   const transaction = [
     {
@@ -86,6 +88,22 @@ const HomeSuperAdmin = () => {
               quantity={pengguna.quantity}
             />
           ))}
+          <div className="rounded-xl border border-stroke bg-white px-3 pt-6 pb-2.5 shadow-default sm:px-7.5 xl:pb-1 ">
+            <button className="bg-[#026C52] h-[45px] w-[45px] rounded-full flex items-center justify-center hover:bg-white hover:outline outline-1 outline-green-600 ">
+              <ArrowForwardIosIcon className="h-[30px] w-[30px] text-white hover:text-green-600" />
+            </button>
+            <p className="text-[10px] text-gray-700   dark:text-gray-400 pt-4">
+              Permintaan Payout
+            </p>
+            <h5 className="mb-2 text-4xl font-bold tracking-tight text-gray-900 ">
+              32
+            </h5>
+            <p className="flex items-center  text-sm font-medium mb-8">
+              <NorthIcon className="text-green-600" />
+              <span className=" px-1 text-[20px] text-green-600">+2%</span>
+              <span className="text-gray-500">than last week</span>
+            </p>
+          </div>
         </div>
 
         <div className="bg-gray-100">
@@ -108,10 +126,7 @@ const HomeSuperAdmin = () => {
             <div className="flex items-center">
               <div className="flex justify-end items-center mr-2 border-2 border-black">
                 <h1 className="font-semibold">Tampilkan</h1>
-                <select className="bg-white text-[#999999] p-1 px-2 py-2 rounded-md outline outline-black outline-1 ml-2">
-                  <option>Data Terbaru</option>
-                  <option>Filter 2</option>
-                </select>
+                <Date />
               </div>
               <button className="bg-[#243775] text-white py-[6px] px-4 rounded-md m-2 flex items-center">
                 <KeyboardArrowDownIcon fontSize="small" className="mr-1" />
@@ -171,9 +186,22 @@ const HomeSuperAdmin = () => {
                     <td className="px-3 py-4 font-reguler text-[#3653B0]">
                       {item.total}
                     </td>
-                    <td className="px-1 py-4 font-reguler text-[#3653B0]">
+                    <td
+                      className={`${
+                        item.status === "berhasil"
+                        ? "bg-[#9EC7BD] py-2 px-6 m-4 w-20 text-[#1F5C4D] items-center flex justify-center rounded-full"
+                        : item.status === "gagal"
+                        ? "bg-red-300 py-2 w-20 px-6 m-4 text-[#9B4332] items-center flex justify-center rounded-full"
+                        : item.status === "diproses"
+                        ? "bg-yellow-300 py-2 w-20 px-6 m-4 text-yellow-800 items-center flex justify-center rounded-full"
+                        : "" 
+                      }`}
+                    >
                       {item.status}
                     </td>
+                    {/* <td className="px-1 py-4 font-reguler text-[#3653B0]">
+                      {item.status}
+                    </td> */}
                   </tr>
                 ))}
               </tbody>
